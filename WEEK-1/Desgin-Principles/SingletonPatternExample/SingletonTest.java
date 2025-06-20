251967fa3df6887c8811f0f26ffcf49e3717c569
joshika k <152405060+joshika202@users.noreply.github.com>
2025-06-20 21:31:01 +0530

    Add files via upload

---
+++ WEEK-1/Desgin-Principles/SingletonPatternExample/SingletonTest.java
@@ -0,0 +1,20 @@
+
+
+public class SingletonTest {
+    public static void main(String[] args) {
+        // Get Logger instance twice
+        Logger logger1 = Logger.getInstance();
+        Logger logger2 = Logger.getInstance();
+
+        // Check if both references point to the same instance
+        if (logger1 == logger2) {
+            System.out.println("Both logger instances are the same (Singleton works).");
+        } else {
+            System.out.println("Logger instances are different (Singleton failed).");
+        }
+
+        // Use the logger
+        logger1.log("This is the first log message.");
+        logger2.log("This is the second log message.");
+    }
+}
