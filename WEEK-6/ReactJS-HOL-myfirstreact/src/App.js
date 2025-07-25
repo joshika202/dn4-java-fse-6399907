57e01b2ad1d01cb179facd95427b907dc95dfcf3
joshika k <152405060+joshika202@users.noreply.github.com>
2025-07-25 22:07:50 +0530

    Add files via upload

---
+++ WEEK-6/ReactJS-HOL-myfirstreact/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <h1>Welcome to the first session of React</h1>
+      <h2>Run the following command to execute the React application:</h2>
+      <p>npm start</p>
+    </div>
+  );
+}
+
+export default App;
+
+
+
